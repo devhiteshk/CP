a9d2deeba317699208d7f7ba7c990a929b0e279c
thexcaliber <hitesh.k.83080@gmail.com>
2022-07-26 16:10:54 +0530

    "Right-Rotate trick"

--- PROBLEMS/L2 TO L3/Indivisible_Permutation.cpp
+++ PROBLEMS/L2 TO L3/Indivisible_Permutation.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-
 int main()
 {
     int t;
@@ -8,6 +8,16 @@ int main()
     while (t--)
     {
         // CODE HERE
+        ll n;
+        cin >> n;
+
+        ll temp = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            std::cout << i + 1 << " ";
+        }
+        std::cout << 1 << std::endl;
     }
     return 0;
 }
