c041ae11b1940e2981999449060f3acf3960f039
thexcaliber <hitesh.k.83080@gmail.com>
2022-08-13 17:34:24 +0530

    "REMONE"

---
+++ PROBLEMS/L2 TO L3/Remove_One_Element.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define siz(x) ((int)x.size())
+#define all(a) ((a).begin(), (a).end())
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+#define cin_arr(i) cin >> arr[i];
+#define cout_arr(i) cout << arr[i];
+#define eol cout << endl;
+#define printVec(vec)                   \
+    for (ll i = 0; i < vec.size(); i++) \
+    {                                   \
+        cout << vec[i] << " ";          \
+    }                                   \
+    eol;
+#define printArr(arr, N)       \
+    for (ll i = 0; i < N; i++) \
+    {                          \
+        cout << arr[i] << " "; \
+    }                          \
+    cout << endl;
+#define cus_for(i, x, k, in) for (ll i = x; i < k; i += in)
+#define cus_rfor(i, x, k, in) for (ll i = x; i >= k; i -= in)
+
+template <typename T>
+ll getMin(T arr[], ll N)
+{
+    ll min = INT_MAX;
+    cus_for(i, 0, N, 1)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+template <typename T>
+ll getMax(T arr[], ll N)
+{
+    ll max = INT_MIN;
+    cus_for(i, 0, N, 1)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void solve()
+{
+    ll N;
+    cin >> N;
+
+    vector<ll> arr1;
+    cus_for(i, 0, N, 1)
+    {
+        ll temp;
+        cin >> temp;
+        arr1.push_back(temp);
+    }
+
+    vector<ll> arr2;
+    cus_for(j, 0, N - 1, 1)
+    {
+        ll temp1;
+        cin >> temp1;
+        arr2.push_back(temp1);
+    }
+
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+   
+    ll x1 = arr2[0] - arr1[0]; 
+    ll x2 = arr2[0] - arr1[1]; 
+
+    int ans = 2;
+    for(int i=1;i<N;i++)
+        {
+            if(arr2[i-1]-arr1[i]!=x2)
+            {
+                ans=1;
+            }
+        }
+        
+        if(ans==2&&x2>0)
+        {
+            cout<<x2<<endl;
+        }
+        else
+        {
+            cout<<x1<<endl;
+        }
+
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+// ---> NEVER GIVE UP
+//                        ▬▬▬▬▬▬▬▬▬▬▬.◙.▬▬▬▬▬▬▬▬▬▬▬
+//                                ▂▄▄▓▄▄▂
+//                              ◢◤██▀▀████▄▄▄▄▄▄▄▄▄▄◢◤
+//                             █▄████▄ ███▀▀▀▀▀▀▀▀▀▀╬
+//                              ◥█████◤
+//                              ══╩══╩══
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬
+//                                ╬═╬  ☻/🏓Have a nice Day !!!!!!!🏓
+//                                ╬═╬ /▌
+//                                ╬═╬ / \
