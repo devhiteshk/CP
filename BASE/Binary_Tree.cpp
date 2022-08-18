a56ab7c935353fcc8f2433deac8a0ff52ced3911
thexcaliber <hitesh.k.83080@gmail.com>
2022-08-18 17:56:29 +0530

    "BST.cpp"

---
+++ BASE/Binary_Tree.cpp
@@ -0,0 +1,18 @@
+//A binary tree is a tree data structure in which each parent node can have at most two children. Each node of a binary tree consists of three items: data item, address of left child & address of right child.
+
+//A full Binary tree is a special type of binary tree in which every parent node/internal node has either two or no children.
+
+//A perfect binary tree is a type of binary tree in which every internal node has exactly two child nodes and all the leaf nodes are at the same level.
+
+//A complete binary tree is just like a full binary tree, but with two major differences
+
+//   Every level must be completely filled
+//  All the leaf elements must lean towards the left.
+  
+//  The last leaf element might not have a right sibling i.e. a complete binary tree doesn't have to be a full binary tree.
+
+//A degenerate or pathological tree is the tree having a single child either left or right.
+
+//A skewed binary tree is a pathological/degenerate tree in which the tree is either dominated by the left nodes or the right nodes. Thus, there are two types of skewed binary tree: left-skewed binary tree and right-skewed binary tree.
+
+//It is a type of binary tree in which the difference between the height of the left and the right subtree for each node is either 0 or 1.
